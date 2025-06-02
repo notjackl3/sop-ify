a826b64f20ef0bedbf7f422d56999959b2cca230
Jack Le <jackl32482005@gmail.com>
2025-06-02 22:04:08 +0700

    deleted unnecessary files

--- dummy.py
+++
@@ -1,7 +0,0 @@
-import fitz  # pip install PyMuPDF
-
-doc = fitz.open("converted.pdf")
-for page_num, page in enumerate(doc):
-    text_instances = page.search_for("YOUR_TARGET_TEXT")
-    for inst in text_instances:
-        print(f"Found on page {page_num + 1} at {inst}")
